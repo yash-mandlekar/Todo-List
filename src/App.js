a1cb145574239acdd2523dddffb7aea18541f86f
yash-mandlekar <91611265+yash-mandlekar@users.noreply.github.com>
2022-06-16 23:04:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import TodoList from './TodoList';
+
+const App = () => {
+  return (
+    <div>
+      <div className='main'>
+        <TodoList/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './App.css'
+
+const TodoList = () => {
+
+    const [list, setList] = useState("");
+    const [item, setItem] = useState([]);
+    const addItem = () => {
+        if (list) {
+            setItem([...item, list])
+            setList("")
+        }
+    }
+    const deleteItem = (id) => {
+        const deleteSingle = item.filter((elem, i) => {
+            return i !== id;
+        })
+        setItem(deleteSingle)
+    }
+
+    return <div className='taskcnt'>
+        <div className='task'>
+        <h2 tabIndex='1'>{item.length} Tasks</h2>
+        <ol>
+            {
+                item.map((elem, i) => {
+                    return (
+                        <div key={i}>
+                        <input type="checkbox" name="" id="" />
+                        {elem}
+                            <button type="button" onClick={() => deleteItem(i)} className="addbtn" >delete</button>
+                        
+                        </div>
+                    )
+                })
+            }
+        </ol>
+            <div className="todocnt">
+                <input value={list}
+                    onChange={(e) => setList(e.target.value)}
+                    type="text" 
+                    id="floatingPassword"
+                    placeholder="Add Todo..."
+                />
+                <button type="button" onClick={addItem} className="addbtn">add</button>
+            </div>
+        </div>
+    </div>;
+};
+
+export default TodoList;
