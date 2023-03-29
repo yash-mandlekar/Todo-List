ee25158f22bdecea6a7cd0984891ebfcff4d95a5
yash <ymandlekar9826@gmail.com>
2023-03-29 21:48:44 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import TodoList from './TodoList';
-
 const App = () => {
+  const ref = React.useRef(null);
+  const handleScroll = () => {
+    ref.current.scrollBy(0, 100);
+  }
+
   return (
-    <div>
-      <div className='main'>
+    <>
         <TodoList/>
-      </div>
-    </div>
+        {/* <div ref={ref} className="box-cnt">
+          <div className="box">1</div>
+          <div className="box">2</div>
+          <div className="box">3</div>
+        </div>
+        <button onClick={handleScroll}>scroll</button> */}
+    </>
   );
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,50 +1,80 @@
-import React, { useState } from 'react';
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 const TodoList = () => {
-    const [list, setList] = useState("");
-    const [item, setItem] = useState([]);
-    const addItem = () => {
-        if (list) {
-            setItem([...item, list])
-            setList("")
-        }
+  const [list, setList] = useState("");
+  const [item, setItem] = useState([
+    { task: "Learn React", isCompleted: false },
+    { task: "Learn Node", isCompleted: false },
+  ]);
+  const addItem = (e) => {
+    e.preventDefault();
+    if (list) {
+      setItem([...item, { task: list, isCompleted: false }]);
+      setList("");
     }
-    const deleteItem = (id) => {
-        const deleteSingle = item.filter((elem, i) => {
-            return i !== id;
-        })
-        setItem(deleteSingle)
-    }
-
-    return <div className='taskcnt'>
-        <div className='task'>
-        <h2>{item.length} Tasks</h2>
-        <ol>
-            {
-                item.map((elem, i) => {
-                    return (
-                        <div key={i}>
-                        <input type="checkbox" name="" id="" />
-                        {elem}
-                            <button type="button" onClick={() => deleteItem(i)} className="addbtn" >delete</button>
-                        
-                        </div>
-                    )
-                })
-            }
-        </ol>
-            <div className="todocnt">
-                <input value={list}
-                    onChange={(e) => setList(e.target.value)}
-                    type="text" 
-                    id="floatingPassword"
-                    placeholder="Add Todo..."
-                />
-                <button type="button" onClick={addItem} className="addbtn">add</button>
+  };
+  const deleteItem = (id) => {
+    const deleteSingle = item.filter((elem, i) => {
+      return i !== id;
+    });
+    setItem(deleteSingle);
+  };
+  const handleCheckbox = (id) => {
+    const check = item.map((elem, i) => {
+      if (i === id) {
+        return { ...elem, isCompleted: !elem.isCompleted };
+      }
+      return elem;
+    });
+    setItem(check);
+  };
+  return (
+    <div className="taskcnt">
+      <div className="task">
+        <div className="header">{item.length} Tasks</div>
+        <div className="todos">
+          {item.map((elem, i) => (
+            <div className={`todo ${i % 2 === 0 ? "grey" : ""}`} key={i}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheckbox(i)}
+                checked={elem.isCompleted}
+              />
+              <p>
+                {elem.isCompleted ? (
+                  <strike>
+                    {i + 1}. {elem.task}
+                  </strike>
+                ) : (
+                  i + 1 + ". " + elem.task
+                )}
+              </p>
+              <button
+                onClick={() => deleteItem(i)}
+                type="button"
+                className="btn"
+              >
+                delete
+              </button>
             </div>
+          ))}
         </div>
-    </div>;
+        <form onSubmit={addItem} className="footer">
+          <input
+            value={list}
+            onChange={(e) => setList(e.target.value)}
+            type="text"
+            id="floatingPassword"
+            placeholder="Add Todo..."
+          />
+          <button type="submit" className="addbtn">
+            +
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default TodoList;
