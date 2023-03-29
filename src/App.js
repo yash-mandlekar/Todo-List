5adab287a44e4395b61ed6adc4ff8ea5782cb178
yash <ymandlekar9826@gmail.com>
2023-03-29 22:02:10 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import TodoList from './TodoList';
 const App = () => {
-  const ref = React.useRef(null);
-  const handleScroll = () => {
-    ref.current.scrollBy(0, 100);
-  }
+  // const ref = React.useRef(null);
+  // const handleScroll = () => {
+  //   ref.current.scrollBy(0, 100);
+  // }
 
   return (
     <>
         <TodoList/>
-        <div ref={ref} className="box-cnt">
+        {/* <div ref={ref} className="box-cnt">
           <div className="box">1</div>
           <div className="box">2</div>
           <div className="box">3</div>
         </div>
-        <button onClick={handleScroll}>scroll</button>
+        <button onClick={handleScroll}>scroll</button> */}
     </>
   );
 }
